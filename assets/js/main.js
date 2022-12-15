0d9e891e4f756eb3b4d91b9bfee189d691f55f1b
xarli <117117311+c4rli@users.noreply.github.com>
2022-12-15 00:53:49 +0000

    Fix resizing bugs, refactor CSS, HTML and JS files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,20 @@
+// Inspiration (reverse engineered) taken from :
+// https://codepen.io/JGallardo/pen/ZEBbeP
+
+
+/* Once window scrolls down the equibalent of 100vh the navbar will appear
+and apply these classes to make the navbar stay at the top and apply 
+fancy transiton color changing effects */
+
 $(window).scroll(function(){
-    if ($(window).scrollTop() >= window.innerHeight  ) {
+    if ($(window).scrollTop() >= window.innerHeight ) {
         $('nav').addClass('fixed-header');
         $('nav img').addClass('fixed-header-img');
-        $('nav').removeClass('navbar-dark');
-        $('nav').removeClass('fixed-header-reverse');
+        $('nav').removeClass('navbar-dark'); 
         $('nav').addClass('navbar-light');
-
+        // $('nav').removeClass('fixed-header-reverse');
     }
+
     // else {
     //     $('nav').removeClass('fixed-header');
     //     $('nav img').removeClass('fixed-header-img');
